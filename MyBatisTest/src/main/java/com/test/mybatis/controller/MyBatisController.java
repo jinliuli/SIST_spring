4c244a81c5573ff1d9aebe2f5707e78bf082fc7d
jinliuli <jinliuli0305@gmail.com>
2024-10-21 17:52:56 +0900

    241021_spring_MyBatis_select문 추가

--- MyBatisTest/src/main/java/com/test/mybatis/controller/MyBatisController.java
+++ MyBatisTest/src/main/java/com/test/mybatis/controller/MyBatisController.java
@@ -1,5 +1,9 @@
 package com.test.mybatis.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -71,6 +75,180 @@ public class MyBatisController {
 		int result = dao.m3(dto);
 		
 		System.out.println(result);
+		
+		return "list";
+	}
+	
+	
+	
+	//반환값(O) > 단일값(1행 1열)
+	@GetMapping(value="/m4.do")
+	public String m4(Model model) {
+	
+		//select count(*) from tblAddress
+		
+		int count = dao.m4();
+		
+		model.addAttribute("count", count);
+		
+		return "list";
+	}
+	
+	
+	//반환값(O) > 단일값(1행 N열)
+	@GetMapping(value="/m5.do")
+	public String m5(Model model, @RequestParam(name="seq", defaultValue="1") Integer seq) {
+		
+		//select * from tblAddress where seq = ?
+		AddressDTO dto = dao.m5(seq);
+		
+		model.addAttribute("dto", dto);
+		
+		return "list";
+	}
+	
+	
+	// 반환값(O) > 단일값(N행 1열)
+	@GetMapping(value = "/m6.do")
+	public String m6(Model model) {
+		
+		//select name from tblAddress
+		List<String> names = dao.m6();
+		
+		model.addAttribute("names", names);
+		
+		return "list";
+	}
+
+	// 반환값(O) > 단일값(N행 N열)
+	@GetMapping(value = "/m7.do")
+	public String m7(Model model) {
+		
+		//select * from tblAddress
+		List<AddressDTO> list = dao.m7();
+		
+		model.addAttribute("list", list);
+		
+		return "list";
+	}
+	
+	@GetMapping(value = "/m8.do")
+	private String m8(Model model, String table) {
+		
+		//m8.do?table=tblAddress
+		//- select count(*) from tblAddress
+		
+		//m8.do?table=tblUser
+		//- select count(*) from tblUser
+		
+		int count = dao.m8(table);
+		
+		model.addAttribute("count", count);
+		
+		return "list";
+	}
+	
+	
+	@GetMapping(value = "/m9.do")
+	private String m9(Model model, int age) {
+		
+		//m9.do?age=25
+		//- select * from tblAddress where age < 25
+		
+		List<AddressDTO> list = dao.m9(age);
+		
+		model.addAttribute("list", list);
+		
+		return "list";
+		
+	}
+	@GetMapping(value = "/m10.do")
+	private String m10(Model model, String word) {
+		
+		//m10.do?word=검색어
+		//- select * from tblAddress where address like '%검색어%'
+		
+		List<AddressDTO> list = dao.m10(word);
+		
+		model.addAttribute("list", list);
+		
+		return "list";
+	}
+	
+	@GetMapping(value = "/m11.do")
+	private String m11(Model model, String gender) {
+		
+		//m11.do?gender=m
+		//m11.do?gender=f
+		//m11.do
+		
+		//동적 쿼리
+		//- select * from tblAddress where gender = 'm'
+		//- select * from tblAddress where gender = 'f'
+		//- select * from tblAddress
+		
+		List<AddressDTO> list = dao.m11(gender);
+		
+		model.addAttribute("list", list);
+		
+		return "list";
+	}
+	
+	@GetMapping(value = "/m13.do")
+/*	private String m13(Model model, HashMap<String, String> map) { 해쉬맵으로 만들기 불가 DTO로 만들던지 해야됨*/ 
+	private String m13(Model model, String gender, String word) {
+		
+		//m13.do
+		//m13.do?gender=f
+		//m13.do?word=역삼
+		//m13.do?gender=f&word=역삼
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("gender", gender);
+		map.put("word", word);
+		
+		List<AddressDTO> list = dao.m13(map);
+		
+		model.addAttribute("list", list);
+		
+		return "list";
+	}
+	
+
+	@GetMapping(value = "/m14.do")
+	private String m14(Model model) {
+		
+		AddressDTO dto = new AddressDTO();
+		dto.setName("사막여우");
+		dto.setAge(3);
+		dto.setGender("f");
+		dto.setAddress("이집트");
+		
+		//방금 insert한 레토드의 seq?
+		//1. insert
+		//2. select max(seq)
+		
+		int result = dao.m14(dto);
+		
+		model.addAttribute("result", result);
+		model.addAttribute("dto", dto);
+		
+		return "list";
+	}
+	@GetMapping(value = "/m15.do")
+	private String m15(Model model) {
+		
+		//주소 검색: 강아지 or 사자
+		//- where name in ('강아지', '사자')
+		
+		List<String> search = new ArrayList<String>();
+		
+		search.add("강아지");
+		search.add("사자");
+		
+		List<AddressDTO> list = dao.m15(search);
+		
+		model.addAttribute("list", list);
 		
 		return "list";
 	}
--- MyBatisTest/src/main/java/com/test/mybatis/dto/AddressDTO.java
+++ MyBatisTest/src/main/java/com/test/mybatis/dto/AddressDTO.java
@@ -2,9 +2,11 @@ package com.test.mybatis.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class AddressDTO {
 	
 	private String seq;
