1f2da86dadd72963608c8090e3f89f4df0bf90f6
jinliuli <jinliuli0305@gmail.com>
2024-10-23 16:46:25 +0900

    241023_spring_AOP_AOP를 어노테이션을 사용해서 설정

---
+++ AOPTest/src/main/java/com/test/aop/aspect/Logger.java
@@ -0,0 +1,22 @@
+package com.test.aop.aspect;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component //스프링<bean>
+@Aspect		//<aop:aspect ref="logger"> 역할 부여
+public class Logger {
+	
+	@Pointcut("execution(public * com.test.aop.dao.AOPDAO.add(..))") 
+	public void pc1() {//바지사장
+		//구현부(X)
+	}
+	
+	@After("pc1()")
+	public void m1() {
+		System.out.println("[LOG] 로그를 작성합니다.");
+	}
+	
+}
