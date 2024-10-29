caafaa321a5f166fbda002edfb6b26a03cb12b7f
jinliuli <jinliuli0305@gmail.com>
2024-10-29 19:48:24 +0900

    241029_AjaxTest..독학하기..

---
+++ AjaxServer/src/main/java/com/test/ajax/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.test.ajax.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserDTO {
+
+	private String id;
+	private String name;
+	private String gender;
+	
+}
+
+
+
+
+
